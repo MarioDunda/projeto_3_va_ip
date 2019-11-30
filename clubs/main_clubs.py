5ad0532e0fdac0d711f3b1bc9cb1c7e6e17c7143
MarioDunda <mario.dunda@live.com>
2019-11-30 18:59:15 -0300

    Update main_clubs.py

--- clubs/main_clubs.py
+++ clubs/main_clubs.py
@@ -1,6 +1,5 @@
 
 
-
 def main_clubs():
     print("""
 [ 1 ] - Jogadores
@@ -11,10 +10,84 @@ def main_clubs():
     menu = input("Digite uma opção: ")
 
     if(menu == "1"):
+        main_player()
+    elif(menu == "2"):
+        main_club()
+    elif(menu == "3"):
+        main_manager
+    else:
         print("""
+
+Digite uma opção valida
+
+        """)
+        main_clubs()
+
+
+def main_player():
+    print("""
+[ 1 ] - Cadastrar novo jogador
+[ 2 ] - Alterar jogador
+[ 0 ] - Voltar
+    """)
+    menu = input("Digite uma opção: ")
+    if(menu == "1"):
+        print("cadastrar")
+    elif(menu == "2"):
+        print("alterar")
+    elif(menu == "0"):
+        main_clubs()
+    else:
+        print("""
+
+Digite uma opção valida
+
+        """)
+        main_player()
+
+
+def main_club():
+    print("""
 [ 1 ] - Cadastrar novo clube
 [ 2 ] - Alterar clube
 [ 0 ] - Voltar
     """)
-        menu = input("Digite uma opção: ")
-        
+    menu = input("Digite uma opção: ")
+    if(menu == "1"):
+        print("cadastrar")
+    elif(menu == "2"):
+        print("alterar")
+    elif(menu == "0"):
+        main_clubs()
+    else:
+        print("""
+
+Digite uma opção valida
+
+        """)
+        main_club()
+
+
+def main_manager():
+    print("""
+[ 1 ] - Cadastrar novo técnico
+[ 2 ] - Alterar técnico
+[ 0 ] - Voltar
+    """)
+    menu = input("Digite uma opção: ")
+    if(menu == "1"):
+        print("cadastrar")
+    elif(menu == "2"):
+        print("alterar")
+    elif(menu == "0"):
+        main_clubs()
+    else:
+        print("""
+
+Digite uma opção valida
+
+        """)
+        main_clubs()
+
+
+main_clubs()
